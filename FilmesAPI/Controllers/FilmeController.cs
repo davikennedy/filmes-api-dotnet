e9b977d5731e216108907926409f19d680587544
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-31 22:44:17 -0300

    Recebendo os dados de um filme

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
