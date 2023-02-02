bfade607aca344d0c7537662a41b55378d2fbcf6
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-01 22:52:00 -0300

    Validando parâmetros recebidos

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,19 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required (ErrorMessage = "Insira o título do filme")]
         public string Titulo { get; set; }
+        
+        [Required (ErrorMessage = "Insira o nome do diretor")]
         public string Diretor { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Range(30, 300, ErrorMessage = "A duração deve ter entre 30 e 300 minutos")]
         public int Duracao { get; set; }
-        public int Ano { get; set; }
     }
 }
