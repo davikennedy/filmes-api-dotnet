5f4c7a2bd288f9e3666a468debfff3ca75bc535a
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-08 22:30:28 -0300

    Atualizando classe Filme

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,23 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FilmesAPI.Models
+namespace FilmesAPI.Models;
+
+public class Filme
 {
-    public class Filme
-    {
-        [Key]
-        [Required]
-        public int Id { get; internal set; }
+    [Key]
+    [Required]
+    public int Id { get; internal set; }
 
-        [Required (ErrorMessage = "Insira o título do filme")]
-        public string Titulo { get; set; }
-        
-        [Required (ErrorMessage = "Insira o nome do diretor")]
-        public string Diretor { get; set; }
+    [Required (ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    
+    [Required (ErrorMessage = "O diretor do filme é obrigatório")]
+    public string Diretor { get; set; }
 
-        [Required]
-        public string Genero { get; set; }
+    [Required]
+    public string Genero { get; set; }
 
-        [Range(30, 300, ErrorMessage = "A duração deve ter entre 30 e 300 minutos")]
-        public int Duracao { get; set; }
-    }
+    [Required]
+    [Range(30, 300, ErrorMessage = "A duração deve ter entre 30 e 300 minutos")]
+    public int Duracao { get; set; }
 }
