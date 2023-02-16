e17f42a4c38d331d3689545d7766a2141712d81b
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-16 08:25:41 -0300

    Relacionando Filme e Cinema por meio de uma Sessao

--- FilmesAPI/Models/Sessao.cs
+++ FilmesAPI/Models/Sessao.cs
@@ -4,16 +4,9 @@ namespace FilmesAPI.Models
 {
     public class Sessao
     {
-        [Key]
-        [Required]
-        public int Id { get; set; }
-
         //Relacionamento 1 : n
-        [Required]
         public int FilmeId { get; set; }        
         public virtual Filme Filme { get; set; }
-
-        [Required]
         public int? CinemaId { get; set; }
         public virtual Cinema Cinema { get; set; }
     }
