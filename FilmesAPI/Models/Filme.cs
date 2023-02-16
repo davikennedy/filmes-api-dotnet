32f289f36b811695f90ead4cf70ff88c328924d3
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-15 22:51:04 -0300

    Relacionando Filme e Sessao

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -20,4 +20,5 @@ public class Filme
     [Required]
     [Range(30, 300, ErrorMessage = "A duração deve ter entre 30 e 300 minutos")]
     public int Duracao { get; set; }
+    public virtual ICollection<Sessao> Sessoes { get; set; }
 }
---
+++ FilmesAPI/Models/Sessao.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Sessao
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        //Relacionamento 1 : n
+        [Required]
+        public int FilmeId { get; set; }        
+        public Filme Filme { get; set; }
+    }
+}
